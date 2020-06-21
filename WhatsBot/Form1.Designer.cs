835ad44ca4e4d51ed2374a051bcd5cc7f1173e96
Marcus Vinicius de Freitas Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2020-06-21 18:27:19 -0300

    Initial

---
+++ WhatsBot/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WhatsBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtConvite = new System.Windows.Forms.TextBox();
+            this.lblConvite = new System.Windows.Forms.Label();
+            this.txtMensagemApp = new System.Windows.Forms.TextBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(145, 368);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(293, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(542, 368);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(116, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtConvite
+            // 
+            this.txtConvite.Location = new System.Drawing.Point(145, 86);
+            this.txtConvite.Name = "txtConvite";
+            this.txtConvite.Size = new System.Drawing.Size(513, 20);
+            this.txtConvite.TabIndex = 2;
+            // 
+            // lblConvite
+            // 
+            this.lblConvite.AutoSize = true;
+            this.lblConvite.Location = new System.Drawing.Point(70, 89);
+            this.lblConvite.Name = "lblConvite";
+            this.lblConvite.Size = new System.Drawing.Size(69, 13);
+            this.lblConvite.TabIndex = 3;
+            this.lblConvite.Text = "Link Convite:";
+            // 
+            // txtMensagemApp
+            // 
+            this.txtMensagemApp.Location = new System.Drawing.Point(145, 124);
+            this.txtMensagemApp.Multiline = true;
+            this.txtMensagemApp.Name = "txtMensagemApp";
+            this.txtMensagemApp.Size = new System.Drawing.Size(513, 110);
+            this.txtMensagemApp.TabIndex = 4;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(77, 124);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(62, 13);
+            this.lblMensagem.TabIndex = 5;
+            this.lblMensagem.Text = "Mensagem:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Enabled = false;
+            this.txtLog.Location = new System.Drawing.Point(145, 240);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(513, 109);
+            this.txtLog.TabIndex = 6;
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(107, 243);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(32, 13);
+            this.lblLog.TabIndex = 7;
+            this.lblLog.Text = "LOG:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 493);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.txtMensagemApp);
+            this.Controls.Add(this.lblConvite);
+            this.Controls.Add(this.txtConvite);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtConvite;
+        private System.Windows.Forms.Label lblConvite;
+        private System.Windows.Forms.TextBox txtMensagemApp;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblLog;
+    }
+}
+
---
+++ WhatsBot/Form1.cs
@@ -0,0 +1,230 @@
+﻿using OpenQA.Selenium;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhatsBot
+{
+
+    public partial class Form1 : Form
+    {
+        private static IWebDriver webDriver;
+        private static bool Prosseguir = false;
+        private delegate void Acao();
+        string conviteUrl = @"https://chat.whatsapp.com/HJYelb7tUfqKjX2AU4whGF";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //conviteUrl = txtConvite.Text;
+            try
+            {
+                webDriver = WebDriverFactory.CreateWebDriver(Browser.Chrome, @"C:\Users\marcu\source\repos\Web Bots\Bot.WhatsApp.Share.Selenium\bin\Debug\netcoreapp2.0", false);
+                Validar(new Acao(AbrirWhatApp));
+                Validar(new Acao(BotaoJuntar));
+                Validar(new Acao(SemDownload));
+                Validar(new Acao(EntrarGrupo));
+                Validar(new Acao(EnviarMensagem));
+                Validar(new Acao(AbrirMenu));
+                Validar(new Acao(SairGrupo));
+                Validar(new Acao(ConfirmarSaida));
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AbrirWhatApp()
+        {
+            try
+            {
+                Console.WriteLine("Abrindo o WhatsApp Web");
+                webDriver.LoadPage(TimeSpan.FromSeconds(10), "https://web.whatsapp.com");
+                Console.WriteLine("Pegue seu celular e leia o QRCODE.");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void BotaoJuntar()
+        {
+            try
+            {
+                Console.WriteLine("Carregando a página de convite.");
+                webDriver.LoadPage(TimeSpan.FromSeconds(40), conviteUrl);
+                //Clicar no botao Join
+                Console.WriteLine("Clicando no botao join.");
+                var btnJoin = webDriver.FindElement(By.Id("action-button"));
+                btnJoin.Click();
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void SemDownload()
+        {
+            try
+            {
+                Console.WriteLine("Sem Download.");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                var btnSemDownload = webDriver.FindElements(By.ClassName("_36or"));
+                btnSemDownload[75].Click();
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+
+        private void EntrarGrupo()
+        {
+            try
+            {
+                //Aguardo o botão Entrar no grupo aparecer
+                Thread.Sleep(TimeSpan.FromSeconds(20));
+                //Clicar no botão entrar no grupo
+                Console.WriteLine("Clicando no botao entrar no grupo.");
+                var btnEntrarNoGrupo = webDriver.FindElement(By.ClassName("PNlAR"));
+                btnEntrarNoGrupo.Click();
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void EnviarMensagem()
+        {
+            try
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(20));
+                //Digitar uma mensagem
+                var txtMensagem = By.XPath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]");
+                webDriver.SetText(txtMensagem, "Ola sou da equipe do WhatsApp, seu grupo foi selecionado para realização de testes de segurança! Pedimos desculpas pelo transtorno. Esse teste deve levar alguns minutos.");
+
+                //Enviamos a mensagem
+                webDriver.FindElement(txtMensagem).SendKeys(OpenQA.Selenium.Keys.Enter);
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void AbrirMenu()
+        {
+            try
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                Console.WriteLine("Abrindo o menu.");
+                var menus = webDriver.FindElements(By.ClassName("rAUz7"));
+                if (menus[5].Displayed)
+                {
+                    menus[5].Click();
+                }
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void SairGrupo()
+        {
+            try
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                Console.WriteLine("Clicando em sair do grupo.");
+                var opcoesDoMenu = webDriver.FindElements(By.ClassName("_34D8D"));
+                if (opcoesDoMenu[4].Displayed)
+                {
+                    opcoesDoMenu[4].Click();
+                }
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+        private void ConfirmarSaida()
+        {
+            try
+            {
+                Console.WriteLine("Aguardando 25 segundos para o botão sair aparacer.");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine("Confirmando saida do grupo.");
+                var btnSairGrupo = webDriver.FindElement(By.ClassName("PNlAR"));
+
+                btnSairGrupo.Click();
+                Console.WriteLine("Operação realizada com sucesso!");
+                Prosseguir = true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $"{ex.Message}\n";
+                Prosseguir = false;
+            }
+        }
+
+
+        private void Validar(Acao acao)
+        {
+            try
+            {
+                while (!Prosseguir)
+                {
+                    acao();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+            finally
+            {
+                Prosseguir = false;
+            }
+        }
+
+    }
+}
